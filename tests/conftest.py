403c7689588d820fe42e8f2e82180a99afd248e8
Oleksandr Samoilenko <Oleksandr.Samoilenko@halliburton.com>
2023-03-28 13:23:08 +0300

    finished tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,8 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def prepare_first_file(tmp_path):
-    first_file = os.path.join(tmp_path, 'file1.txt')
+def prepare_normal_first_file(tmp_path):
+    first_file = os.path.join(tmp_path, 'prepare_normal_first_file.txt')
     with open(first_file, 'w') as file:
         lines = ['same line 1\n',
                  'same line 2\n',
@@ -23,8 +23,8 @@ def prepare_first_file(tmp_path):
 
 
 @pytest.fixture(autouse=True)
-def prepare_second_file(tmp_path):
-    second_file = os.path.join(tmp_path, 'file2.txt')
+def prepare_normal_second_file(tmp_path):
+    second_file = os.path.join(tmp_path, 'prepare_normal_second_file.txt')
     with open(second_file, 'w') as file:
         lines = ['same line 1\n',
                  'same line 2\n',
@@ -40,23 +40,65 @@ def prepare_second_file(tmp_path):
                  ]
         file.writelines(lines)
     return second_file
+
 
 @pytest.fixture(autouse=True)
-def prepare_reader(tmp_path):
-    second_file = os.path.join(tmp_path, 'file2.txt')
+def prepare_full_same_first_file(tmp_path):
+    second_file = os.path.join(tmp_path, 'prepare_full_same_first_file.txt')
     with open(second_file, 'w') as file:
-        lines = ['same line 1\n',
-                 'same line 2\n',
-                 'same line 3\n',
-                 'diff SECOND FILE 1\n',
-                 'same line 4\n',
-                 'diff SECOND FILE 2\n',
+        lines = ['same line 3\n',
                  'same line 1\n',
                  'same line 3\n',
-                 'diff SECOND FILE 3\n',
+                 'same line 4\n',
+                 'same line 1\n',
                  'same line 2\n',
-                 'diff SECOND FILE 4\n'
+                 'same line 3\n',
+                 'same line 2\n',
+                 'same line 4\n',
                  ]
         file.writelines(lines)
     return second_file
 
+
+@pytest.fixture(autouse=True)
+def prepare_full_same_second_file(tmp_path):
+    second_file = os.path.join(tmp_path, 'prepare_full_same_second_file.txt')
+    with open(second_file, 'w') as file:
+        lines = ['same line 1\n',
+                 'same line 2\n',
+                 'same line 4\n',
+                 'same line 3\n',
+                 'same line 4\n',
+                 'same line 1\n',
+                 'same line 3\n',
+                 'same line 2\n',
+                 'same line 3\n',
+                 ]
+        file.writelines(lines)
+    return second_file
+
+
+@pytest.fixture(autouse=True)
+def prepare_full_diff_first_file(tmp_path):
+    second_file = os.path.join(tmp_path, 'prepare_full_same_second_file.txt')
+    with open(second_file, 'w') as file:
+        lines = ['diff FIRST FILE 1\n',
+                 'diff FIRST FILE 2\n',
+                 'diff FIRST FILE 3\n'
+                 ]
+        file.writelines(lines)
+    return second_file
+
+
+@pytest.fixture(autouse=True)
+def prepare_full_diff_second_file(tmp_path):
+    second_file = os.path.join(tmp_path, 'prepare_full_diff_second_file.txt')
+    with open(second_file, 'w') as file:
+        lines = ['diff SECOND FILE 1\n',
+                 'diff SECOND FILE 2\n',
+                 'diff SECOND FILE 3\n',
+                 'diff SECOND FILE 4\n',
+                 'diff SECOND FILE 5\n',
+                 ]
+        file.writelines(lines)
+    return second_file
--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -1,12 +1,51 @@
 from FileReader import FileReader
 
 
-def test_getting_same(prepare_first_file, prepare_second_file):
+def test_getting_same(prepare_normal_first_file, prepare_normal_second_file):
     expected = ['same line 1\n',
                 'same line 2\n',
                 'same line 3\n',
                 'same line 4\n']
-    reader = FileReader(prepare_first_file, prepare_second_file)
+    reader = FileReader(prepare_normal_first_file, prepare_normal_second_file)
     same = reader.get_same()
     assert set(same) == set(expected)
 
+
+def test_getting_diff(prepare_normal_first_file, prepare_normal_second_file):
+    expected = ['diff FIRST FILE 1\n',
+                'diff FIRST FILE 2\n',
+                'diff FIRST FILE 3\n',
+                'diff FIRST FILE 4\n',
+                'diff SECOND FILE 1\n',
+                'diff SECOND FILE 2\n',
+                'diff SECOND FILE 3\n',
+                'diff SECOND FILE 4\n']
+    reader = FileReader(prepare_normal_first_file, prepare_normal_second_file)
+    diff = reader.get_difference()
+    assert set(diff) == set(expected)
+
+
+def test_getting_full_diff(prepare_full_diff_first_file, prepare_full_diff_second_file):
+    expected_same = []
+    expected_diff = ['diff FIRST FILE 1\n',
+                     'diff FIRST FILE 2\n',
+                     'diff FIRST FILE 3\n',
+                     'diff SECOND FILE 1\n',
+                     'diff SECOND FILE 2\n',
+                     'diff SECOND FILE 3\n',
+                     'diff SECOND FILE 4\n',
+                     'diff SECOND FILE 5\n']
+    reader = FileReader(prepare_full_diff_first_file, prepare_full_diff_second_file)
+    same = reader.get_same()
+    diff = reader.get_difference()
+    assert set(expected_same) == set(same)
+    assert  len(set(diff)) > 0
+
+
+def test_getting_full_same(prepare_full_same_first_file, prepare_full_same_second_file):
+    expected_diff = []
+    reader = FileReader(prepare_full_same_first_file, prepare_full_same_second_file)
+    same = reader.get_same()
+    diff = reader.get_difference()
+    assert len(set(same)) > 0
+    assert set(expected_diff) == set(diff)
